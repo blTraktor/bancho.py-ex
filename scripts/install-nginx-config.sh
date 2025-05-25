daaa7c47fc1116c4af13d6fd31b0a2b2ac39a809
KozacheShapka <kozacheshapka@gmail.com>
2025-05-25 18:31:35 +0000

    a small fix

--- scripts/install-nginx-config.sh
+++ scripts/install-nginx-config.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 echo "Sourcing environment from .env file"
 set -a
-source .env
+source ../.env
 set +a
 
 echo "Installing nginx configuration"
-envsubst '${APP_PORT},${DOMAIN},${SSL_CERT_PATH},${SSL_KEY_PATH},${DATA_DIRECTORY}' < ext/nginx.conf.example > /etc/nginx/sites-available/bancho.conf
+envsubst '${APP_PORT},${DOMAIN},${SSL_CERT_PATH},${SSL_KEY_PATH},${DATA_DIRECTORY}' < ../ext/nginx.conf.example > /etc/nginx/sites-available/bancho.conf
 ln -f -s /etc/nginx/sites-available/bancho.conf /etc/nginx/sites-enabled/bancho.conf
 
 echo "Restarting nginx"
